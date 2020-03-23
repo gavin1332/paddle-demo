be61ce9594b1366d38fb22534a7e4575d8a47e59
Yi Liu <gavin1332@gmail.com>
2020-03-23 17:17:28 +0800

    Update fleet_api.py

--- fleet/fleet_api.py
+++ fleet/fleet_api.py
@@ -17,6 +17,7 @@ v1 = fluid.layers.collective._c_allgather(y, fleet.worker_num(), use_calc_stream
 v2 = fluid.layers.collective._c_allreduce(y, use_calc_stream=True)
 fluid.layers.Print(v1)
 fluid.layers.Print(v2)
+# end of testing code
 
 cost = fluid.layers.square_error_cost(y, label)
 loss = fluid.layers.reduce_sum(cost)
@@ -28,12 +29,12 @@ strategy.collective_mode = "grad_allreduce"
 optimizer = fleet.distributed_optimizer(optimizer, strategy=strategy) # new line 5
 optimizer.minimize(loss, fluid.default_startup_program())
 
-place = fluid.CUDAPlace(0) # to be commented line 1
+#place = fluid.CUDAPlace(0) # to be commented line 1
 place = fluid.CUDAPlace(int(os.environ['FLAGS_selected_gpus'])) # uncomment line 1
 exe = fluid.Executor(place)
 exe.run(fluid.default_startup_program())
 
-train_prog = fluid.default_main_program() # to be commented line 2
+#train_prog = fluid.default_main_program() # to be commented line 2
 train_prog = fleet.main_program # uncomment line 2
 x_data = np.ones(shape=[1, 2], dtype=np.float32)
 label_data = np.ones(shape=[1, 1], dtype=np.float32)
